c33a51a85a85db942ca68cbeb9bfe84ccf05b724
Abdul Bari <addubari@gmail.com>
2017-08-25 15:32:17 +0530

    Add Mult

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,50 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@R1
+D=M
+@integer1
+M=D     // integer1 = R1
+
+
+@R2
+D=M
+@integer2
+M=D     // integer2 = R2
+
+
+@sum
+M=0   // sum = 0
+
+@i
+M=1   // i = 1
+(LOOP)  
+    @i
+    D=M
+    @integer2
+    D=D-M
+    @STOP
+    D;JGT   // if i > integer2 goto STOP
+
+    @sum
+    D=M
+    @integer1
+    D=D+M  
+    @sum
+    M=D // Add integer1 to sum
+
+    @i
+    M=M+1
+    @LOOP
+    0;JMP
+
+(STOP)
+    @sum
+    D=M
+    @R0
+    M=D    
+
+(END)
+    @END
+    0;JMP
